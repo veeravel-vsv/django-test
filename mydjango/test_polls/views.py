2986ba81fdd15fa8972e6ebf1b45d463edf0fe4b
veeravela <aveera94@gmail.com>
2016-11-15 17:39:52 +0530

    commit1

--- mydjango/test_polls/views.py
+++ mydjango/test_polls/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpRequest, HttpResponse
+from .models import LoginUser
 # Create your views here.
 
 def index(request,users_id):
-	return HttpResponse("Hello! Django %s" % users_id)
+	usr_id = get_object_or_404(LoginUser, pk = users_id)
+	return render(request, 'polls/index.html',{'a':usr_id})
+	# return render(request, 'polls/index.html', {'a':users_id})
