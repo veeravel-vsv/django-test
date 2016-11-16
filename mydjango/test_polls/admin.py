6e12351f5e86636c8cd64b679a71fea8b3e6024b
veeravela <aveera94@gmail.com>
2016-11-16 20:25:33 +0530

    commit update2

--- mydjango/test_polls/admin.py
+++ mydjango/test_polls/admin.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import LoginUser
+# from .forms import LoginForm
 admin.site.register(LoginUser)
+# admin.site.register(LoginForm)
--- mydjango/test_polls/views.py
+++ mydjango/test_polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpRequest, HttpResponse
 from .models import LoginUser
+# from .forms import LoginForm
 # Create your views here.
 
 def login(request):
@@ -9,11 +10,11 @@ def login(request):
 	# return render(request, 'polls/index.html', {'a':users_id})
 
 def index(request):
-	# if request.method == "POST":
-	# 	login_usr = LoginUser(request.POST)
-	# 	if login_usr.is_valid():
-	# 		usr_name = login_usr.cleaned_data['usrText']
-	# else:
-	# 	login_usr = LoginUser()
-	login_usr = LoginUser(request.POST)
+	if request.method == "POST":
+		login_usr = LoginUser(request.POST)
+		if login_usr.is_valid():
+			usr_name = login_usr.cleaned_data['usrText']
+	else:
+		login_usr = LoginUser()
+	# login_usr = LoginUser(request.POST)
 	return render(request, 'polls/index.html',{'usr_name':login_usr})
