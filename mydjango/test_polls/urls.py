62214a1d0fc76f2db5b7cdbc9eee7b9c00edcf24
veeravela <aveera94@gmail.com>
2016-11-16 20:00:57 +0530

    commit11/16

--- mydjango/test_polls/urls.py
+++ mydjango/test_polls/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
 from . import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
-					url(r'^(?P<users_id>[0-9]+)/$', views.index, name = 'index'),
+					url(r'^$', views.login, name = 'login'),
+					# url(r'^login/',TemplateView.as_view(template_name="login.html")),
+					url(r'^index/', views.index, name = 'index_name'),				
 			  ]
--- mydjango/test_polls/views.py
+++ mydjango/test_polls/views.py
@@ -3,7 +3,17 @@ from django.http import HttpRequest, HttpResponse
 from .models import LoginUser
 # Create your views here.
 
-def index(request,users_id):
-	usr_id = get_object_or_404(LoginUser, pk = users_id)
-	return render(request, 'polls/index.html',{'a':usr_id})
+def login(request):
+	# usr_id = get_object_or_404(LoginUser, pk = users_id)
+	return render(request, 'polls/login.html')
 	# return render(request, 'polls/index.html', {'a':users_id})
+
+def index(request):
+	# if request.method == "POST":
+	# 	login_usr = LoginUser(request.POST)
+	# 	if login_usr.is_valid():
+	# 		usr_name = login_usr.cleaned_data['usrText']
+	# else:
+	# 	login_usr = LoginUser()
+	login_usr = LoginUser(request.POST)
+	return render(request, 'polls/index.html',{'usr_name':login_usr})
